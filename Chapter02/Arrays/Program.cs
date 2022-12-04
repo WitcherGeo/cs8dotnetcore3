b016107eff8f88c42528559e13639cd4ec8dec72
WitcherGeo <ggyagyaev1@gmail.com>
2022-12-04 11:42:25 +0300

    Add Fityres

--- Chapter02/Arrays/Program.cs
+++ Chapter02/Arrays/Program.cs
@@ -9,15 +9,14 @@ namespace Arrays
       string[] names; // can reference any array of strings
 
       // allocating memory for four strings in an array
-      names = new string[4];
+      names = new string[5];
 
       // storing items at index positions
       names[0] = "Kate";
       names[1] = "Jack";
       names[2] = "Rebecca";
       names[3] = "Tom";
-
-      // looping through the names
+    names[5] = "George";
       for (int i = 0; i < names.Length; i++)
       {
         // output the item at index position i
